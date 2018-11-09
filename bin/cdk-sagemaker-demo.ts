f59d51b217a5f24e51c3ab5ec9044314b6020081
Matt McClean <mmcclean@amazon.com>
2018-11-09 17:46:44 +0000

    added new main cdk file

--- bin/cdk-sagemaker-demo.ts
+++ bin/cdk-sagemaker-demo.ts
@@ -1,24 +1,20 @@
 #!/usr/bin/env node
-import sns = require('@aws-cdk/aws-sns');
-import sqs = require('@aws-cdk/aws-sqs');
 import cdk = require('@aws-cdk/cdk');
+import { SageMakerNotebook } from './sagemakernb';
 
 class CdkSagemakerDemoStack extends cdk.Stack {
   constructor(parent: cdk.App, name: string, props?: cdk.StackProps) {
     super(parent, name, props);
 
-    const queue = new sqs.Queue(this, 'CdkSagemakerDemoQueue', {
-      visibilityTimeoutSec: 300
+    /** Create the SageMaker notebook instance */
+    new SageMakerNotebook(this, 'MySageMakerNotebook', {
+      onCreateScriptPath: 'scripts/onCreate.sh',
+      onStartScriptPath: 'scripts/onStart.sh'
     });
 
-    const topic = new sns.Topic(this, 'CdkSagemakerDemoTopic');
-
-    topic.subscribeQueue(queue);
   }
 }
 
 const app = new cdk.App();
-
 new CdkSagemakerDemoStack(app, 'CdkSagemakerDemoStack');
-
 app.run();
